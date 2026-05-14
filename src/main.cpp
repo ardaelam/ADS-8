864e0092695190c27e7a6b674eb3ccea31cc07e7
ardaelam <artem.elesin2007@gmail.com>
2026-05-14 13:22:23 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
+#include <iostream>
+
+void makeTree(BST<std::string>& tree, const char* filename);
+void printFreq(BST<std::string>& tree);
 
 int main() {
+    BST<std::string> tree;
+    const char* filename = "src/war_peace.txt";
+    makeTree(tree, filename);
+
+    std::cout << "Глубина дерева: " << tree.depth() << std::endl;
+    std::cout << "Частота 'pierre': " << tree.search("pierre") << std::endl;
+    std::cout << "Частота 'natasha': " << tree.search("natasha") << std::endl;
+    std::cout << "Частота 'andrew': " << tree.search("andrew") << std::endl;
+
+    printFreq(tree);
     return 0;
 }
